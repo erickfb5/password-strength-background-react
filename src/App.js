6037fea2a14c32c0edb908b117b4f0cac227693e
Erick Brandao <brandaoerick@MacBook-Air.local>
2023-02-28 17:00:52 -0300

    first version

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+
+import "./App.css";
+
+const App = () => {
+  const [password, setPassword] = useState("");
+  const [blurValue, setBlurValue] = useState(20);
+
+  const handlePasswordChange = (e) => {
+    const input = e.target.value;
+    const length = input.length;
+    const newBlurValue = 20 - length * 2;
+    setBlurValue(newBlurValue);
+    setPassword(input);
+  };
+
+  return (
+    <div>
+      <div
+        className="background"
+        id="background"
+        style={{ filter: `blur(${blurValue}px)` }}
+      ></div>
+      <div className="bg-white rounded p-10 text-center shadow-md">
+        <h1 className="text-3xl">Image Password Strength</h1>
+        <p className="text-sm text-gray-700">
+          Change the password to see the effect
+        </p>
+        <div className="my-4 text-left">
+          <label htmlFor="email" className="text-gray-900">
+            Email:
+          </label>
+          <input
+            type="text"
+            className="border block w-full p-2 mt-2 rounded"
+            id="email"
+            placeholder="Enter Email"
+          />
+        </div>
+
+        <div className="my-4 text-left">
+          <label htmlFor="password" className="text-gray-900">
+            Password:
+          </label>
+          <input
+            type="password"
+            className="border block w-full p-2 mt-2 rounded"
+            id="password"
+            placeholder="Enter Password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </div>
+
+        <button
+          className="bg-black text-white py-2 mt-4 inline-block w-full rounded"
+          type="submit"
+        >
+          Submit
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default App;
